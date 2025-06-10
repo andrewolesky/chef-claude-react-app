8cb5600c238d2be0f31fc028c252bb22303791dd
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-10 16:01:32 -0400

    Added header and main form.

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "/images/logo_v4.jpg";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chefClaudeLogo} />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Header from "../components/Header";
+import Main from "../components/Main";
+
 export default function App() {
-  return <>This is App!</>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
