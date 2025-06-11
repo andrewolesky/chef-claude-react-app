55041ffe4a92ad3e0d2b7bca35a4dc782ea7e5af
Andrew Olesky <oleskyinbox@gmail.com>
2025-06-11 17:59:17 -0400

    React markdown package added for formatting the recipe.

--- components/HFRecipe.jsx
+++ components/HFRecipe.jsx
@@ -1,3 +1,10 @@
+import ReactMarkdown from "react-markdown";
+
 export default function HFRecipe(props) {
-  return <section>{props.recipe}</section>;
+  return (
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>Chef AI Recommends:</h2>
+      <ReactMarkdown>{props.recipe}</ReactMarkdown>
+    </section>
+  );
 }
